f2ad6ebb4d50b69d1227b75d94a50bac6a00200d
iDarkSSJ <joseluiscasadiego0826@gmail.com>
2026-02-09 16:02:01 -0500

    Created react project

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-green-700 text-5xl font-bold">WORKPAGE</h1>
+      <h2>MIGUEL</h2>
+      <h2>JOSE LUIS</h2>
+      <h2>JERLINSON</h2>
+    </div>
+  )
+}
+
+export default App
